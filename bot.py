d0331f210e4ed73003c4a590fde0b840820dc83d
Sn6ok <maynerpro220@gmail.com>
2025-03-24 20:48:13 +0200

    Обнова5

--- bot.py
+++ bot.py
@@ -1,1 +1,7 @@
 import telebot
+
+bot = telebot.TeleBot('7978854259:AAFx7lEricXAZBMFWVE0D8W3WgbWph5CIaY')
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, 'Привіт, пострижись!')
